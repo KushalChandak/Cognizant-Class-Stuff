39ccc24be1486b8b7f5cd45dc4d324dfe39341e2
KushalChandak <kpchandak500@gmail.com>
2020-03-17 15:15:06 +0530

    Add files via upload

---
+++ CodeChallange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter Number :");
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\t");
+
+            Console.WriteLine("\t{0}{0}{0} \n", n);
+            Console.WriteLine("\t{0}{0} \n", n);
+            Console.WriteLine("\t{0}{0}\n", n);
+            Console.WriteLine("\t{0}{0}\n", n);
+            Console.WriteLine("\t{0}{0}{0}\n", n);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
